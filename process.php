20fa3967f2a2cc04d0d1413cb33b54c27629951a
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-12-20 20:27:32 -0800

    return process form values

--- process.php
+++ process.php
@@ -1,3 +1,13 @@
 <?php
-var_dump($_POST);
+$name = $_POST["name"];
+$email = $_POST["email"];
+$suggestion = $_POST["suggest"];
+
+echo "<pre>";
+$email_body = "";
+$email_body .= "Name: " . $name . "\n";
+$email_body .= "Email: " . $email . "\n";
+$email_body .= "Suggestion: " . $suggestion . "\n";
+echo $email_body;
+echo "</pre>";
 ?>
