606b0ec15a695469e7a70bb97a647de452ea2bed
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-12-20 19:30:20 -0800

    Suggestions / process
    Suggestions / process

--- suggest.php
+++ suggest.php
@@ -7,7 +7,30 @@ include("inc/header.php");
 ?>
 
 <div class="section page">
-  <h1>Create a New Shelf</h1>
+<div class="wrapper">
+  <h1>Send me a message</h1>
+  <p>Let me know if you think there is something missing</p>
+
+    <form method="post" action="process.php">
+    <table>
+      <tr>
+        <th><label for="name">Name</label></th>
+        <td><input type="text" name="name" id="name"></td>
+      </tr>
+      <tr>
+        <th><label for="email">Email</label></th>
+        <td><input type="text" name="email" id="email"></td>
+      </tr>
+      <tr>
+        <th><label for="suggest">Suggestion Details</label></th>
+        <td><textarea name="suggest" id="suggest" cols="30" rows="10"></textarea></td>
+      </tr>
+    </table>
+    <input type="submit" value="Send" />
+    
+            
+    </form>
+  </div>
 </div>
 
 <?php includ("inc/footer.php"); ?>
